98f914e48146ca76169babaf2ef0765440ffec0d
daniel <danielbaradaran31-hash>
2025-12-12 21:00:24 +0100

    feat: add navigation icons to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,6 +16,16 @@ const Navbar = () => {
                 ))}
             </ul>
         </div>
+
+        <div>
+            <ul>
+                {navIcons.map((  {id, img} ) => (
+                    <li key={id}>
+                        <img src={img} className="icon-hover" alt={`icon-${id}`}/>
+                    </li>
+                ))}
+            </ul>
+        </div>
     </nav>
   )
 }
