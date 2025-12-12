df41f7fae97e20c29a6df12691f23c2e757ce91a
daniel <danielbaradaran31-hash>
2025-12-12 22:09:19 +0100

    feat: fix import statement syntax in Navbar component and enhance Welcome component with text rendering

--- src/constants/Welcome.jsx
+++ src/constants/Welcome.jsx
@@ -1,15 +1,34 @@
-import React, { use } from 'react'
+import { useRef } from 'react';
 
+const FONT WEIGHTS = {
+   subtitle: { min: 100, max: 400 , default: 100 },
+    title: { min: 400, max: 900, default: 400 },
+};
+
+const renderText = (text, className, baseWeight = 400) =>{
+return [...text].map((char, i) => (
+    <span key={i} className={className} style={{fontVariationSettings: `'whgt ${baseWeight}`}}>
+        {char== "" ? '\u00A0' : char}</span>
+));
+}
+
+const setupTextHover = (container, type ) => {
+    if(!container) return;
+
+    const letters = container.querySelectorAll('span');
 const Welcome = () => {
     const titleRef = useRef(null);
     const subtitleRef = useRef(null);
   return (
     <section id='welcome'>
-     <p ref={subtitleRef}>Hey , Ich bin Daniel! Willkommen in mein</p>
-        <h1 ref={titleRef} className='mt-7'>Portfolio</h1>
+     <p ref={subtitleRef}>
+        {renderText("Hey , Ich bin Daniel! Willkommen in mein", 'text-3xl font-georama',100)}
+        </p>
+        <h1 ref={titleRef} className='mt-7'>
+            {renderText("Portfolio", 'text-9xl italic font-georama')}</h1>
 
         <div className='smal-screen'>
-            <p>Das ist designed für desktop/tabled nur screens.</p>
+            <p>Das ist designed für desktop/tabled nur screens</p>
         </div>
     </section>
   )
