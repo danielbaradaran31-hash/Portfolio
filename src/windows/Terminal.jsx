26b3b09956e649b9d8827b4a00dc29f6b15059cc
daniel <danielbaradaran31-hash>
2025-12-14 15:58:25 +0100

    update

--- src/windows/Terminal.jsx
+++ src/windows/Terminal.jsx
@@ -20,7 +20,29 @@ const Terminal = () => {
     </div>
 
     <ul className="content">
-        {techStack.map(({ category, technologies }) => (
+        {techStack.map(({ category, items }) => (
+            <li key={category} className="flex items-center">
+                <Check className="check" size={20} />
+                <h3>{ category }</h3>
+                <ul>
+                    { items.map((item, i) => (
+                        <li key={i}>{item}
+                        {i < items.length - 1 ? "," : ""}</li>
+                    ))}
+                </ul>
+
+                <div className="footnote">
+                    <p>
+                        <Check size={20} />5 of 5 stacks loaded successfully (100%)
+                    </p>
+
+                    <p className="text-black">
+                        <Flag size={15} fill= "black"/>
+                        Render time: 6ms
+                    </p>
+                </div>
+            </li>
+        ))}
     </ul>
   </div>
   
